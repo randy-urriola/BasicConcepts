77c0acde1a951ffd8435325a5c81e552ba555c82
randy-urriola <randy.urriolah@gmail.com>
2026-06-10 14:50:44 -0500

    proyexto 2 - constantes y declaraciones con var

---
+++ Circle/Program.cs
@@ -0,0 +1,12 @@
+﻿var radio = 0d;
+var resultadoCirculo = 0d;
+
+const double Pi = 3.14;
+
+Console.WriteLine("Cálculo del área del circulo");
+Console.WriteLine("Ingrese el valor del radio:");
+radio = Convert.ToDouble(Console.ReadLine());
+
+resultadoCirculo = Pi * radio * radio;
+
+Console.WriteLine("El radio del círculo es: " + radio + ", el área es: " + resultadoCirculo);
