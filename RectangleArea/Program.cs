2ee7f3d89d03237e405d647c8f7b4809c5fb1421
randy-urriola <randy.urriolah@gmail.com>
2026-06-10 10:34:59 -0500

    Add project files.

---
+++ RectangleArea/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Programa, area del rectangulo
+double ladoA, ladoB, resultado;
+
+Console.WriteLine("Calcula el Área de un Rectangulo!");
+Console.WriteLine("Ingresa el valor del lado A: ");
+ladoA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingresa el valor del lado B: ");
+ladoB = Convert.ToDouble(Console.ReadLine());
+
+resultado = ladoA * ladoB;
+
+Console.WriteLine("El lado A: " + ladoA + " por El lado B: " + ladoB +  " Resulta en: " + resultado);
+
+
+// Para convertir a int puede ser Convert.ToInt32() o Convert.ToInt64()
